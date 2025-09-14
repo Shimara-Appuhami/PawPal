366ce7b91024d3a378fc323324184393c5363ff3
Shimara-Appuhami <shimarailshani@gmail.com>
2025-09-14 16:01:17 +0530

    get storage

--- firebase.ts
+++ firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,9 +12,10 @@ const firebaseConfig = {
   authDomain: "pawpal-28f28.firebaseapp.com",
   databaseURL: "https://pawpal-28f28-default-rtdb.firebaseio.com",
   projectId: "pawpal-28f28",
-  storageBucket: "pawpal-28f28.firebasestorage.app",
+  storageBucket: "gs://pawpal-28f28.firebasestorage.app",
   messagingSenderId: "363119788687",
-  appId: "1:363119788687:web:af8b4c7914078ff6d9a6fa"
+  appId: "1:363119788687:web:af8b4c7914078ff6d9a6fa",
+  
 };
 
 // Initialize Firebase
@@ -21,3 +23,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export const storage = getStorage(app)
