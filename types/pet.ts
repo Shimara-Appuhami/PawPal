96420d7cbcccac409894cb6ad0fc07981a0c304a
Shimara-Appuhami <shimarailshani@gmail.com>
2025-09-15 00:22:07 +0530

    add image

--- types/pet.ts
+++ types/pet.ts
@@ -5,4 +5,7 @@ export interface Pet {
   name: string;      // Pet name
   age: string;       // Pet age (can be string or number depending on your input)
   type: string;      // Pet type (Dog, Cat, etc.)
+
+  breed: string;     // Pet breed (e.g., Labrador, Persian)
+  imageUrl?: string; // Public URL to the pet image stored in Firebase Storage
 }
