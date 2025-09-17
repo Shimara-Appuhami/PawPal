db60145704a3050a8157f64cd144b88511941a15
Shimara-Appuhami <shimarailshani@gmail.com>
2025-09-18 01:12:33 +0530

    fix layout

--- app/(dashboard)/task/_layout.tsx
+++ app/(dashboard)/task/_layout.tsx
@@ -1,19 +1,9 @@
-import { Slot } from "expo-router";
-import React from "react";
-import { View } from "react-native";
+import { Stack } from "expo-router";
 
 export default function TaskLayout() {
   return (
-    <View className="flex-1 bg-white">
-      {/* header */}
-      {/* <View className="bg-orange-500 p-4 flex-row justify-between items-center">
-        <Text className="text-white text-lg font-bold">Pet Tasks</Text>
-        <TouchableOpacity onPress={() => router.back()}>
-          <Text className="text-white font-semibold">Back</Text>
-        </TouchableOpacity>
-      </View> */}
-
-      <Slot />
-    </View>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+    </Stack>
   );
 }
